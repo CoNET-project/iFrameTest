b9c0bfad6e94c98cc2f8bcb288e56c0c7baa6cfe
CoNET Project <info@CoNETTech.ca>
2024-02-03 22:04:00 -0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import { Button } from 'antd';
+import Start from './components/Home/index';
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <Start></Start>
+    </div>
+  );
+}
+
+export default App;
